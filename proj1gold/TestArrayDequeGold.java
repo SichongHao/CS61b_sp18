310a389c08f458f2830408bb1eadc58cdc74ae85
SichongHao <haosichong@qq.com>
2022-02-21 21:10:06 +0800

    fixed NullPointerException

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,9 +7,9 @@ public class TestArrayDequeGold {
     public void testADG() {
         StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
-        String failureMessage = "";
+        String failureMessage = new String();
 
-        for (int i = 0; i < 10; i += 1) {
+        for (int i = 0; i < 50; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
 
             if (numberBetweenZeroAndOne > 0.75) {
@@ -23,13 +23,13 @@ public class TestArrayDequeGold {
             } else if (numberBetweenZeroAndOne > 0.25) {
                 Integer actualStudent = sad.removeFirst();      // 实际值
                 Integer expectedSolution = ads.removeFirst();   // 期望值
-                failureMessage = failureMessage + "removeFirst()\n";
                 assertEquals(failureMessage, expectedSolution, actualStudent);
+                failureMessage = failureMessage + "removeFirst()\n";
             } else {
                 Integer actualStudent = sad.removeLast();      // 实际值
                 Integer expectedSolution = ads.removeLast();   // 期望值
-                failureMessage = failureMessage + "removeLast()\n";
                 assertEquals(failureMessage, expectedSolution, actualStudent);
+                failureMessage = failureMessage + "removeLast()\n";
             }
         }
 
